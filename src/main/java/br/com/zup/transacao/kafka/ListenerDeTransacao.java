025498215739355d7e252ccb643fb99f616b480c
vyrs <vitory.rsilva@hotmail.com>
2021-03-26 09:53:52 -0300

    removendo dependencia não utilizada

--- src/main/java/br/com/zup/transacao/kafka/ListenerDeTransacao.java
+++ src/main/java/br/com/zup/transacao/kafka/ListenerDeTransacao.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
-import javax.transaction.Transactional;
-
 @Component
 public class ListenerDeTransacao {
 
